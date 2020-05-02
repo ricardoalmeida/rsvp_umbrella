504f0ad7eb9c489988347e43156740ae4d726742
Ricardo Almeida <ricardoluiz1.almeida@gmail.com>
2020-05-02 21:46:50 +0200

    create channels to update quantiyu

--- apps/rsvp_web/lib/rsvp_web/channels/user_socket.ex
+++ apps/rsvp_web/lib/rsvp_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule RsvpWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", RsvpWeb.RoomChannel
+  channel "event:*", RsvpWeb.EventChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
--- apps/rsvp_web/lib/rsvp_web/controllers/event_controller.ex
+++ apps/rsvp_web/lib/rsvp_web/controllers/event_controller.ex
@@ -35,7 +35,8 @@ defmodule RsvpWeb.EventController do
   end
 
   def reserve(conn, %{"id" => id, "reservation" => %{"quantity" => quantity}}) do
-    Rsvp.EventQueries.decrease_quantity(id, quantity)
+    {:ok, event} = Rsvp.EventQueries.decrease_quantity(id, quantity)
+    RsvpWeb.EventChannel.send_update(event)
     redirect(conn, to: Routes.event_path(conn, :show, id))
   end
 end
